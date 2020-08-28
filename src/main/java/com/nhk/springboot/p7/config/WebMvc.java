e5d80dcda40db2c5439d38da5f42a786106e7aaf
emmardo <emmardo@posteo.net>
2020-08-28 10:38:15 +0100

    Added WebMvc configuration

---
+++ src/main/java/com/nhk/springboot/p7/config/WebMvc.java
@@ -0,0 +1,30 @@
+package com.nhk.springboot.p7.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableSpringDataWebSupport
+public class WebMvc implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**",
+                "/min/**")
+
+
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+}
